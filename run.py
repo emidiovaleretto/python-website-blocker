fdc403dbd44e06940190ca85375e264a2ceb1a94
Emidio Valereto <emidio.valereto@gmail.com>
2021-11-04 23:05:10 +0000

    Create a function that reads the list of blocked sites and return them.

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 from datetime import datetime, timedelta
 
+
+def get_list_of_blocked_websites():
+    """
+    This function returns list of blocked websites.
+    """
+    with open("blocked_sites.txt", "r") as f:
+        blocked_websites = f.readlines()
+
+    return blocked_websites
+
+
 end_date = datetime.now() + timedelta(days=365)
+sites_to_block = get_list_of_blocked_websites()

9951d4fc3b423e89a492bdbb7b65afdcf29d5009
Emidio Valereto <emidio.valereto@gmail.com>
2021-11-04 23:13:26 +0000

    Create .env file to secure sensitive data.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from decouple import config
 
 
 def get_list_of_blocked_websites():
@@ -13,3 +14,6 @@ def get_list_of_blocked_websites():
 
 end_date = datetime.now() + timedelta(days=365)
 sites_to_block = get_list_of_blocked_websites()
+
+hosts_path = config("HOSTS_PATH")
+redirect_to = config("REDIRECT_TO")
